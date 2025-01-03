972c658f08de54ab61385d05e1e2cd96e4dac65b
nikola369nikolaev <nikola_nikolaev1@abv.bg>
2025-01-03 23:39:44 +0200

    initial commit

---
+++ Cards Game/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * You will be given two hands of cards, which will be represented by integers. Assume each one is held by a different player. You have to find which one has the winning deck. You start from the beginning of both hands of cards. Compare the cards from the first deck to the cards from the second deck. The player, who holds the more powerful card on the current iteration, takes both cards and puts them at the back of his hand - the second player's card is placed last and the first person's card (the winning one) comes after it (second to last). If both players' cards have the same values - no one wins and the two cards must be removed from both hands. The game is over only when
+
+one of the decks is left without any cards. You have to display the result on the console and the sum of the remaining cards: "{First/Second} player wins! Sum: {sum}".
+
+Examples
+
+Input Output
+
+20 30 40 50
+
+10 20 30 40 First player wins! Sum: 240
+
+10 20 30 40 50
+
+50 40 30 30 10 Second player wins! Sum: 50
+ */
+
+List<int> firstPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+while (firstPlayer.Count!=0 && secondPlayer.Count!=0)
+{
+    
+    if (firstPlayer[0] > secondPlayer[0])
+    {
+            firstPlayer.Add(secondPlayer[0]);
+            firstPlayer.Add(firstPlayer[0]);
+    }
+    else if (firstPlayer[0] < secondPlayer[0])
+    {
+        secondPlayer.Add(firstPlayer[0]);
+        secondPlayer.Add(secondPlayer[0]);
+    }
+    
+    secondPlayer.RemoveAt(0);
+    firstPlayer.RemoveAt(0);
+}
+
+int firstPlayerSum = firstPlayer.Sum();
+int secondPlayerSum = secondPlayer.Sum();
+
+if (firstPlayerSum > secondPlayerSum)
+{
+    Console.WriteLine($"First player wins! Sum: {firstPlayerSum}");
+}
+else if (secondPlayerSum > firstPlayerSum)
+{
+    Console.WriteLine($"Second player wins! Sum: {secondPlayerSum}");
+}
---
+++ House Party/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Create a program that keeps track of the guests that are going to a house party. On the first line, of input you are going to receive the number of commands that will follow.
+
+On the next lines, you are going to receive some of the following: "{name} is going!"
+
+· You have to add the person, if they are not on the guestlist already.
+
+· If the person is on the list print the following to the console: "{name} is already in the list!"
+
+"{name} is not going!"
+
+· You have to remove the person, if they are on the list.
+
+· If not, print out: "{name} is not in the list!"
+
+Finally, print all of the guests, each on a new line.
+ */
+
+int guestsCount = int.Parse(Console.ReadLine());
+
+List<string> guestList = new List<string>();
+
+for (int i = 0; i < guestsCount; i++)
+{
+    string[] arguments = Console.ReadLine().Split();
+    string name = arguments[0];
+    
+    if (arguments[2] == "going!")
+    {
+        if (guestList.Exists(x => x==name))
+        {
+            Console.WriteLine($"{name} is already in the list!");
+        }
+        else
+        {
+            guestList.Add(name);
+        }
+    }
+    else if (arguments[2]=="not")
+    {
+        if (guestList.Contains(name))
+        {
+            guestList.Remove(name);
+        }
+        else
+        {
+            Console.WriteLine($"{name} is not in the list!");
+        }
+    }
+}
+Console.WriteLine(string.Join("\n",guestList));
